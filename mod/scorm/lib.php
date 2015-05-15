4b936912b71183f07fcf794a940b97e3a2a98af1
Mark Ward <me@moodlemark.com>
2015-05-15 09:48:31 +0100

    Enabled FEATURE_GROUPS for mod_scorm

--- mod/scorm/lib.php
+++ mod/scorm/lib.php
@@ -1001,7 +1001,7 @@ function scorm_pluginfile($course, $cm, $context, $filearea, $args, $forcedownlo
  */
 function scorm_supports($feature) {
     switch($feature) {
-        case FEATURE_GROUPS:                  return false;
+        case FEATURE_GROUPS:                  return true;
         case FEATURE_GROUPINGS:               return false;
         case FEATURE_MOD_INTRO:               return true;
         case FEATURE_COMPLETION_TRACKS_VIEWS: return true;
