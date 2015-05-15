1c4b56f3f6b41f10c157dc38d8559445c0f2448a
Mark Ward <me@moodlemark.com>
2015-05-15 09:49:31 +0100

    Added group access check on each user report for mod_scorm

--- mod/scorm/report/userreporttracks.php
+++ mod/scorm/report/userreporttracks.php
@@ -51,6 +51,9 @@ require_login($course, false, $cm);
 $contextmodule = context_module::instance($cm->id);
 require_capability('mod/scorm:viewreport', $contextmodule);
 
+// Check user has group access
+scorm_require_group_access($cm, $course, $contextmodule, $userid);
+
 // Trigger a tracks viewed event.
 $event = \mod_scorm\event\tracks_viewed::create(array(
     'context' => $contextmodule,
